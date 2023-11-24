8b5d1475deb3b517f584092693a015db086c85df
vacostaepfl <valerie.costa@epfl.ch>
2023-11-24 14:38:24 +0100

    commit for Adrian

--- src/solvers.py
+++ src/solvers.py
@@ -36,7 +36,7 @@ def coupled_solve(
     Returns:
         tuple: A tuple containing two NumPy arrays - x1 and x2, which are solutions to the optimization problem.
     """
-    l22_loss = (1 / 2) * SquaredL2Norm(dim=op.dim_out).asloss(y)
+    l22_loss = (1 / 2) * SquaredL2Norm(op.dim_out).asloss(y)
     F = l22_loss * hstack([op.phi, op.phi])
 
     if lambda2 != 0.0:
@@ -83,27 +83,59 @@ def decoupled_solve(
     lambda2 *= np.linalg.norm(op.phi.adjoint(y), ord=np.inf)
     lambda1 *= np.linalg.norm(op.phi.adjoint(y), ord=np.inf)
 
-    l22_loss = (1 / 2) * QuadraticFunc(
-        (1, op.dim_out),
-        Q=(lambda2 * IdentityOp(op.dim_out) + op.phi.cogram()).dagger(
-            damp=0
-        ),  # mettre Id (N^2 !!!!!) l1/l2 *(N^2 + l2)
-    ).asloss(y)
-    F = l22_loss * op.phi
+    # Co-Gram operator = Indentity ?
+    cogram_id = np.allclose(
+        op.phi.cogram().asarray(), op.dim_in / 2 * np.eye(op.dim_out)
+    )
 
-    F.diff_lipschitz = F.estimate_diff_lipschitz(method="svd")
+    if cogram_id:
+        if isinstance(l2operator, LinOp):
+            raise NotImplementedError
+            # l22_loss = (1 / 2) * QuadraticFunc(
+            #     (1, op.dim_out),
+            #     Q=op.phi
+            #     * l2operator.gram()
+            #     * op.phi.adjoint()  # op.phi.T ??
+            #     * (
+            #         IdentityOp(op.dim_out)
+            #         + lambda2
+            #         * op.phi
+            #         * l2operator.gram()
+            #         * op.phi.adjoint()  # op.phi.T ??
+            #     ).dagger(damp=0),
+            # ).asloss(y)
+
+        else:
+            l22_loss = (1 / 2) * SquaredL2Norm(op.dim_out).asloss(y)
+
+    else:
+        if isinstance(l2operator, LinOp):
+            raise NotImplementedError
+
+        else:
+            l22_loss = (1 / 2) * QuadraticFunc(
+                (1, op.dim_out),
+                Q=(lambda2 * IdentityOp(op.dim_out) + op.phi.cogram()).dagger(damp=0),
+            ).asloss(y)
 
     if lambda1 == 0.0:
         G = NullFunc(op.dim_in)
+    elif cogram_id and not isinstance(l2operator, LinOp):
+        G = lambda1 / lambda2 * (lambda2 + op.dim_in / 2) * L1Norm(op.dim_in)
     else:
         G = lambda1 / lambda2 * L1Norm(op.dim_in)
+
+    F = l22_loss * op.phi
+    F.diff_lipschitz = F.estimate_diff_lipschitz(method="svd")
 
     pgd = PGD(f=F, g=G, verbosity=500)
     sc = MaxIter(n=100) & RelError(eps=1e-4)
     pgd.fit(x0=np.zeros(op.dim_in), stop_crit=sc)
     x1 = pgd.solution()
-    x2 = -op.phi.pinv(op.phi.apply(x1) - y, damp=lambda2)
+    if isinstance(l2operator, LinOp):
+        x2 = -(op.phi.gram() + lambda2 * l2operator.gram()).pinv(
+            op.phi.adjoint(op.phi.apply(x1) - y)
+        )
+    else:
+        x2 = -op.phi.pinv(op.phi.apply(x1) - y, damp=lambda2)
     return x1, x2
-
-
-# print(op.expr())
